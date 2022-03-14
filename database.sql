486e105d9cd6c115962bfc72b52fab4660287460
DemonKingSwarn <rockingswarn@gmail.com>
2022-03-14 14:23:56 +0530

    new table added

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
 create database student;
 use student;
 create table s1(Rno int, SName varchar(15), FName varchar(15), MName varchar(15), Address varchar(40), Phone int, Email varchar(20));
-
+create table s2(Rno int, SName varchar(15), Class varchar(15), Section varchar(15), TotalMarks int, Percentage float, Grade varchar(10));
